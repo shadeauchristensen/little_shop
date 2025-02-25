37d5a584e745f8f62b5bdd85e8cb1f6c153c92f5
Sebastian McKee <sebmcke@gmail.com>
2025-02-25 16:05:42 -0600

    Create item and merchant controllers

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,3 +1,9 @@
-class ItemsController < ApplicationController
-    
+class Api::V1::ItemsController < ApplicationController
+    def index
+        render json: ItemSerializer.new(Item.all)
+    end
+
+    def show
+        render json: ItemSerializer.new(Item.find(params[:id]))
+    end
 end
--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,3 +1,9 @@
-class MerchantsController < ApplicationController
-    
+class Api::V1::MerchantsController < ApplicationController
+    def index
+        render json: MerchantSerializer.new(Merchant.all)
+    end
+
+    def show
+        render json: MerchantSerializer.new(Merchant.find(params[:id]))
+    end
 end
