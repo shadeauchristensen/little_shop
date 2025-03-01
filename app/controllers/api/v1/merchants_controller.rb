fbdeb9a1459a33d4baade4616071c2be80eae0b2
Shadeau Christensen <octogrim-mac@Shadeaus-MacBook-Air-Octogrim.local>
2025-02-28 20:23:36 -0800

    Added happy/sad paths for finding merchants name (caseinsensitive).

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -19,6 +19,28 @@ class Api::V1::MerchantsController < ApplicationController
     def update
         render json: Merchant.update(params[:id], merchant_params)
     end
+
+    def find
+        if params[:name].blank?
+            render json: { error: "Name should not be empty" }, status: :bad_request
+            return
+        end
+
+        begin
+            # Rails.logger.info "Received name parameter: #{params[:name].inspect}"
+
+            merchant = Merchant.where( "name ILIKE ?", "%#{params[:name]}%" ).order(:name).first
+
+            if merchant.nil?
+                render json: { data: {} }, status: :ok
+            else
+                render json: MerchantSerializer.new(merchant), status: :ok
+            end
+
+        rescue StandardError => e
+            render json: { error: "Something went wrong: #{e.message}" }, status: :internal_server_error
+        end
+    end
     
     private
 
---
+++ spec/requests/api/v1/merchants/merchant_search_request_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+RSpec.describe "Merchant Search Request", type: :request do
+    describe "/api/v1/merchants/find" do
+        before :each do
+            @merchant1 = create(:merchant, name: "Turing")
+            @merchant2 = create(:merchant, name: "Ring World")
+            @merchant3 = create(:merchant, name: "NonExistent")
+        end
+
+        it "Happy path, finds first merchant the meets search terms (case-insensitive)" do
+            get "/api/v1/merchants/find?name=ring"
+
+            expect(response).to be_successful
+
+            parsed = JSON.parse(response.body, symbolize_names: true)
+
+            expect(parsed[:data]).to have_key(:id)
+            expect(parsed[:data][:attributes][:name]).to eq("Ring World")
+        end
+
+
+        it "Sad path, returns empty if there is no merchant found" do
+            get "/api/v1/merchants/find?name=unknown"
+
+            expect(response).to be_successful
+
+            parsed = JSON.parse(response.body, symbolize_names: true)
+
+            expect(parsed).to eq({ data: {} })
+        end
+
+        it "Edge case, returns a 400 error if the name parameter is missing" do
+            get "/api/v1/merchants/find"
+    
+            expect(response).to have_http_status(:bad_request)
+            parsed = JSON.parse(response.body, symbolize_names: true)
+    
+            expect(parsed[:error]).to eq("Name should not be empty")
+        end
+    end
+end
