9e31c6217d3bce1e9e972f6034ccdb2f16a30287
ontruster74 <ontruster74@gmail.com>
2025-03-04 20:49:56 -0700

    add customer-invoice model tests

--- spec/requests/api/v1/items/items_request_spec.rb
+++ spec/requests/api/v1/items/items_request_spec.rb
@@ -1,33 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "Merchant's items API", type: :request do
-    describe "GET /api/v1/merchants/{{merchant_id}}/items" do
-        it "Returns the Merchants Items from merchant_id" do
-            merchant = create(:merchant)
-            items = create_list(:item, 3, merchant: merchant)
-
-            get "/api/v1/merchants/#{merchant.id}/items"
-
-            expect(response).to be_successful
-
-            parsed = JSON.parse(response.body, symbolize_names: true)
-
-            expect(parsed[:data].count).to eq(3)
-            expect(parsed[:data].first[:attributes][:merchant_id]).to eq(merchant.id)
-        end
-
-        it "returns a 404 error if the merchant does not exist" do
-            get "/api/v1/merchants/999999/items"
-
-            expect(response).to have_http_status(:not_found)
-
-            parsed = JSON.parse(response.body, symbolize_names: true)
-
-            expect(parsed[:error]).to eq("Merchant not found")
-        end
-    end
-end
-
 RSpec.describe "Items API", type: :request do
     describe "GET /api/v1/items" do
         it "returns all items in the table" do
@@ -111,6 +83,34 @@ RSpec.describe "Items API", type: :request do
             expect(parsed_error[:message]).to be_a(String)
             expect(parsed_error[:errors]).to be_a(Array)
             expect(parsed_error[:errors][0]).to be_a(String)
+        end
+    end
+end
+
+RSpec.describe "Merchant's items API", type: :request do
+    describe "GET /api/v1/merchants/{{merchant_id}}/items" do
+        it "Returns the Merchants Items from merchant_id" do
+            merchant = create(:merchant)
+            items = create_list(:item, 3, merchant: merchant)
+
+            get "/api/v1/merchants/#{merchant.id}/items"
+
+            expect(response).to be_successful
+
+            parsed = JSON.parse(response.body, symbolize_names: true)
+
+            expect(parsed[:data].count).to eq(3)
+            expect(parsed[:data].first[:attributes][:merchant_id]).to eq(merchant.id)
+        end
+
+        it "returns a 404 error if the merchant does not exist" do
+            get "/api/v1/merchants/999999/items"
+
+            expect(response).to have_http_status(:not_found)
+
+            parsed = JSON.parse(response.body, symbolize_names: true)
+
+            expect(parsed[:error]).to eq("Merchant not found")
         end
     end
 end
--- spec/requests/api/v1/merchants/merchant_request_spec.rb
+++ spec/requests/api/v1/merchants/merchant_request_spec.rb
@@ -1,32 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "Item's Merchant API", type: :request do
-    describe "GET /api/v1/items/:id/merchant" do
-        it "Returns the merchant for a given item" do
-        merchant = create(:merchant)
-        item = create(:item, merchant: merchant)
-
-        get "/api/v1/items/#{item.id}/merchant"
-
-        expect(response).to be_successful
-
-        parsed = JSON.parse(response.body, symbolize_names: true)
-
-        expect(parsed[:data][:id].to_i).to eq(merchant.id)
-        end
-
-        it "returns a 404 error if the item does not exist" do
-        get "/api/v1/items/999999/merchant"
-
-        expect(response).to have_http_status(:not_found)
-
-        parsed = JSON.parse(response.body, symbolize_names: true)
-
-        expect(parsed[:error]).to eq("Item not found")
-        end
-    end
-end
-
 RSpec.describe "Merchant API", type: :request do
     describe "GET /api/v1/merchants" do
         it "Returns all merchants in the table" do
@@ -121,4 +94,90 @@ RSpec.describe "Merchant API", type: :request do
         end
     end
     
+end
+
+RSpec.describe "Merchant's Customers API", type: :request do
+    describe "GET /api/v1/merchants/:merchant_id/customers" do
+        it "Returns the Merchants Customers from merchant_id" do
+            merchant = create(:merchant)
+            customer = create(:customer)
+            invoices = create_list(:invoice, 3, customer: customer, merchant: merchant)
+
+            get "/api/v1/merchants/#{merchant.id}/customers"
+
+            expect(response).to be_successful
+
+            parsed = JSON.parse(response.body, symbolize_names: true)
+
+            expect(parsed[:data].count).to eq(1)
+            expect(parsed[:data].first[:type]).to eq('customer')
+        end
+
+        it "returns a 404 error if the merchant does not exist" do
+            get "/api/v1/merchants/999999/customers"
+
+            expect(response).to have_http_status(:not_found)
+
+            parsed = JSON.parse(response.body, symbolize_names: true)
+
+            expect(parsed[:error]).to eq("Merchant not found")
+        end
+    end
+end
+
+RSpec.describe "Merchant's Invoices API", type: :request do
+    describe "GET /api/v1/merchants/:merchant_id/invoices" do
+        it "Returns the Merchants Invoices from merchant_id" do
+            merchant = create(:merchant)
+            customer = create(:customer)
+            invoices = create_list(:invoice, 3, customer: customer, merchant: merchant)
+
+            get "/api/v1/merchants/#{merchant.id}/invoices"
+
+            expect(response).to be_successful
+
+            parsed = JSON.parse(response.body, symbolize_names: true)
+
+            expect(parsed[:data].count).to eq(3)
+            expect(parsed[:data].first[:type]).to eq('invoice')
+            expect(parsed[:data].first[:attributes][:merchant_id]).to eq(merchant.id)
+        end
+
+        it "returns a 404 error if the merchant does not exist" do
+            get "/api/v1/merchants/999999/invoices"
+
+            expect(response).to have_http_status(:not_found)
+
+            parsed = JSON.parse(response.body, symbolize_names: true)
+
+            expect(parsed[:error]).to eq("Merchant not found")
+        end
+    end
+end
+
+RSpec.describe "Item's Merchant API", type: :request do
+    describe "GET /api/v1/items/:id/merchant" do
+        it "Returns the merchant for a given item" do
+        merchant = create(:merchant)
+        item = create(:item, merchant: merchant)
+
+        get "/api/v1/items/#{item.id}/merchant"
+
+        expect(response).to be_successful
+
+        parsed = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed[:data][:id].to_i).to eq(merchant.id)
+        end
+
+        it "returns a 404 error if the item does not exist" do
+        get "/api/v1/items/999999/merchant"
+
+        expect(response).to have_http_status(:not_found)
+
+        parsed = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed[:error]).to eq("Item not found")
+        end
+    end
 end
